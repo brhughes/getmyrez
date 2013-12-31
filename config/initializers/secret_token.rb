6623d52cdcf5337fc44b59022ba9c3b9bcbca124
Brad Hughes <bradrh07@gmail.com>
2013-12-30 23:25:30 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Getmyrez::Application.config.secret_key_base = '07fcf1ef79aaec5eef325815e99796aa6b576f38476f04bf7208c5c31b3990d9870bcb64b41bd9a9939e690d6d7ce7d530f3a0890ed865a54287e1f73841a0ef'
